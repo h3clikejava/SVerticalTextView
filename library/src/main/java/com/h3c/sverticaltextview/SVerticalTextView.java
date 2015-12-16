f7209bf7237d16348c0275bdb6bdd3b828a78246
H3c <h3clikejava@gmail.com>
2015-12-16 16:53:13 +0800

    init

---
+++ library/src/main/java/com/h3c/sverticaltextview/SVerticalTextView.java
@@ -0,0 +1,18 @@
+package com.h3c.sverticaltextview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by H3c on 12/16/15.
+ */
+public class SVerticalTextView extends TextView {
+    public SVerticalTextView(Context context) {
+        super(context);
+    }
+
+    public SVerticalTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
