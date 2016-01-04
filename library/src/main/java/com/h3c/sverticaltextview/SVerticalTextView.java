8bb98de18086eef02498f577e1aa58a67cb1b9f3
H3c <h3clikejava@gmail.com>
2016-01-04 18:18:40 +0800

    支持字符串换行

--- library/src/main/java/com/h3c/sverticaltextview/SVerticalTextView.java
+++ library/src/main/java/com/h3c/sverticaltextview/SVerticalTextView.java
@@ -21,18 +21,20 @@ import java.io.File;
  */
 public class SVerticalTextView extends View {
     private final static String TAG = "SVerticalTextView";
-    private final static boolean IS_DEBUG = false;
+    private final static boolean IS_DEBUG = true;
 
     private static float DENSITY;// 屏幕密度
-    private static int ROW_HEIGHT;// 行高
+    private static int ROW_HEIGHT;// 行高 ，现在程序里设定为字号的0.5倍
     private static int COLUMN_WIDTH;// 列宽
+    private static char NEW_LINE_CHAR = '\n';
 
     private boolean isVertical = false;// 是否垂直显示文本
     private boolean isShadow = false;// 文字阴影
     private String mText;// 文本
     private int mTextColor = Color.BLACK;// 文本颜色
     private int shadowColor = Color.WHITE;// 阴影颜色
-    private float mMaxTextSize;// 最大文字大小[目前没有用到]
+    private float mMaxTextSize;// 最大文字大小
+    private int mMixTextSize = 10;// 最小文字大小
     private TextPaint mTextPaint;
 
     public SVerticalTextView(Context context) {
@@ -63,7 +65,6 @@ public class SVerticalTextView extends View {
         mTextPaint.setTextSize(((mMaxTextSize > 0) ? mMaxTextSize : 16 * DENSITY));
         setTextColor(mTextColor);
 
-        ROW_HEIGHT = (int)(0 * DENSITY);
         COLUMN_WIDTH = (int)(0 * DENSITY);
     }
 
@@ -159,41 +160,13 @@ public class SVerticalTextView extends View {
         int height = viewHeight - paddingTop - paddingBottom;
 
         // 根据View大小测量文字大小
-        float FONT_SIZE = resetTextSize(width, height, mText, mTextPaint);
+        Integer[] rowInfo = new Integer[2];// ［列数，行数］
+        float FONT_SIZE = resetTextSize(width, height, mText, mTextPaint, rowInfo);
         if(width == 0 || height == 0 || FONT_SIZE == 0) return;
 
         // 文字基线［目前没有用到］
 //        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
 //        float baseLineHeight = fontMetrics.descent;
-
-        // 一行显示字数
-        int COUNT_IN_ROW = (int) (width / FONT_SIZE + COLUMN_WIDTH);// 一行显示的字数
-        // 一列显示字数
-        int COUNT_IN_COLUMN = (int) (height / (FONT_SIZE + ROW_HEIGHT));// 一列显示的字数
-        // 一行真正显示的字数
-        int REAL_COUNT_IN_ROW;
-        if(isVertical) {
-            REAL_COUNT_IN_ROW = (int) Math.ceil(mText.length() / (float)COUNT_IN_COLUMN);
-            if(REAL_COUNT_IN_ROW > COUNT_IN_ROW) {
-                REAL_COUNT_IN_ROW = COUNT_IN_ROW;
-            }
-        } else {
-            REAL_COUNT_IN_ROW = (mText.length() < COUNT_IN_ROW) ? mText.length() : COUNT_IN_ROW;
-        }
-        // 一列真正显示的字数
-        int REAL_COUNT_IN_COLUMN;
-        if(isVertical) {
-            REAL_COUNT_IN_COLUMN = (mText.length() < COUNT_IN_COLUMN) ? mText.length() : COUNT_IN_COLUMN;
-        } else {
-            REAL_COUNT_IN_COLUMN = (int) Math.ceil(mText.length() / (float)COUNT_IN_ROW);
-            if(REAL_COUNT_IN_COLUMN > COUNT_IN_COLUMN) {
-                REAL_COUNT_IN_COLUMN = COUNT_IN_COLUMN;
-            }
-        }
-
-        // xy上的偏移，用来把文字居中
-        int CENTER_X_OFFSET = (isVertical ? -1 : 1) * getTextOffset(width, FONT_SIZE, REAL_COUNT_IN_ROW);
-        int CENTER_Y_OFFSET = getTextOffset(height, FONT_SIZE, REAL_COUNT_IN_COLUMN);
 
         if(IS_DEBUG) {
 //            // 绘制文字大小
@@ -207,29 +180,40 @@ public class SVerticalTextView extends View {
 
         // 分段绘制文字
         char[] chars = mText.toCharArray();
-        int index = 0;// 当前索引
-        int INIT_Y = (int)(mTextPaint.getTextSize() + paddingTop - 4 * DENSITY);// 第一个文字的y值[文字底部的值]
-        int viewEnd = isVertical ? (width + paddingLeft) : paddingLeft;
 
         // 绘制阴影
         if(isShadow) {
             mTextPaint.setShadowLayer(15, 0, 0, shadowColor);
         }
 
+        Point lastCharPoint = new Point(
+                isVertical ? (int)(viewWidth - FONT_SIZE) : 0, // 上一个文字的X
+                0// 上一个文字的Y[文字底部的值]
+        );
+        int XOffset;
+        if(isVertical) {
+            XOffset = - (int)(paddingRight + (width - rowInfo[0] * FONT_SIZE - (rowInfo[0]- 1) * ROW_HEIGHT) / 2);
+        } else {
+            XOffset = (int)(paddingLeft + (width - rowInfo[0] * FONT_SIZE - (rowInfo[0]- 1) * COLUMN_WIDTH) / 2);
+        }
+        int YOffset = (int)(mTextPaint.getTextSize() - 4 * DENSITY) + paddingTop
+                + (int)(((height - rowInfo[1] * FONT_SIZE - (rowInfo[1] - 1) * (isVertical ? COLUMN_WIDTH : ROW_HEIGHT)) / 2));
         for (char c: chars) {
-            Point point;
-            if(isVertical) {
-                point = measureCharVerticalPosition(index++, viewEnd, COUNT_IN_COLUMN, INIT_Y, FONT_SIZE);
+            // 先画文字
+            if(NEW_LINE_CHAR == c) {// 换行刷新位子，不画文字
             } else {
-                point = measureCharHorizontalPosition(index++, viewEnd, COUNT_IN_ROW, INIT_Y, FONT_SIZE);
+                // 画文字
+                canvas.drawText(String.valueOf(c), lastCharPoint.x + XOffset, lastCharPoint.y + YOffset, mTextPaint);
             }
+            // 刷新文字位子
+            lastCharPoint = measureCharHorizontalPosition(c,
+                    lastCharPoint.x, lastCharPoint.y,
+                    0, ((int)(0.5 * FONT_SIZE)),
+                    isVertical, (int)FONT_SIZE,
+                    (isVertical ? viewWidth : viewWidth - paddingRight), viewHeight - paddingBottom);
 
-            if(point != null) {
-                // 绘制文字
-                canvas.drawText(String.valueOf(c), point.x + CENTER_X_OFFSET, point.y + CENTER_Y_OFFSET, mTextPaint);
-            } else {
-                Log("ERROR:" + c);
-            }
+            // 越界或者画完了
+            if(lastCharPoint == null) break;
         }
 
         Log("w:" + width + " h:" + height + " pl:" + paddingLeft + "  t:" + mText + "===" + mMaxTextSize+"===" +mTextPaint.getTextSize()+ "===");
@@ -242,11 +226,11 @@ public class SVerticalTextView extends View {
      * @param text
      * @param paint
      */
-    private float resetTextSize(int vWidth, int vHeight, String text, TextPaint paint) {
+    private float resetTextSize(int vWidth, int vHeight, String text, TextPaint paint, Integer[] rowInfo) {
         if(paint == null || vWidth < 1 || vHeight < 1 || TextUtils.isEmpty(text)) return 0;
 
         if(reMeasureTextSize) {
-            int textsize = measureTextSize(vWidth, vHeight, text);
+            int textsize = measureTextSize(vWidth, vHeight, text, rowInfo);
             paint.setTextSize(textsize);
             return textsize;
         }
@@ -262,29 +246,56 @@ public class SVerticalTextView extends View {
      * @param height
      * @param text
      */
-    private int measureTextSize(int width, int height, String text) {
+    private int measureTextSize(int width, int height, String text, Integer[] rowInfo) {
         double area = width * height;
-        int count = text.length();
-        float fontArea = (float)((area - ((COLUMN_WIDTH + ROW_HEIGHT) * count)) / count);
-        float averageSideWidth = (float) Math.sqrt(fontArea);// 平均文字大小
+        int count;// 区域内总文字数
+        int minInRow = -1;// 行最小字数
+        int minInColumn = -1;// 列最小字数
 
-        if(mMaxTextSize > 0 && averageSideWidth > mMaxTextSize) {
-            averageSideWidth = mMaxTextSize;
+        String[] tmpNewLineTexts = text.split("\\n");
+        int maxTextCountInColumn = tmpNewLineTexts.length;// 最大行数
+        int maxTextCountInARow = 0;// 最大列数
+        for (String rowStrInText : tmpNewLineTexts) {
+            if(maxTextCountInARow < rowStrInText.length()) {
+                maxTextCountInARow = rowStrInText.length();
+            }
+        }
+
+        count = maxTextCountInColumn * maxTextCountInARow;
+        if(isVertical) {
+            minInColumn = maxTextCountInARow;
+            rowInfo[1] = maxTextCountInARow;
+            rowInfo[0] = maxTextCountInColumn;
+        } else {
+            minInRow = maxTextCountInARow;
+            rowInfo[0] = maxTextCountInARow;
+            rowInfo[1] = maxTextCountInColumn;
+        }
+
+        float fontArea = (float)(area / count);// 文字区域大小
+        float averageSideWidth = (float) Math.sqrt(fontArea);// 平均文字大小 [这个文字大小包括了字间距]
+        // 行号设定为字间距的0.5
+        ROW_HEIGHT = (int)(averageSideWidth / 3);
+        float realFontWidth = averageSideWidth - ROW_HEIGHT;
+
+        // 文字大小不允许超过设定的最大文字大小
+        if(mMaxTextSize > 0 && realFontWidth > mMaxTextSize) {
+            realFontWidth = mMaxTextSize;
         }
 
         boolean flag = true;
         while (flag) {
-            int countInRow = (int)(width / (averageSideWidth + COLUMN_WIDTH));
-            int countInColumn = (int)(height / (averageSideWidth + ROW_HEIGHT));
+            int countInRow = (int)(width / (realFontWidth + (isVertical ? ROW_HEIGHT : COLUMN_WIDTH)));
+            int countInColumn = (int)(height / (realFontWidth + (isVertical ? COLUMN_WIDTH : ROW_HEIGHT)));
             int tmpFontCount = (countInRow * countInColumn);
-            if(tmpFontCount >= count) {
+            if(tmpFontCount >= count && countInRow > minInRow && countInColumn > minInColumn) {
                 flag = false;
             } else {
-                averageSideWidth -= DENSITY;
+                realFontWidth -= DENSITY;
             }
         }
 
-        return (int) averageSideWidth;
+        return (int) (realFontWidth > mMixTextSize ? realFontWidth : mMixTextSize);
     }
 
     /**
@@ -299,22 +310,54 @@ public class SVerticalTextView extends View {
     /**
      * 测量文字绘制的位置
      */
-    private Point measureCharVerticalPosition(int index, int fontStartXInView, int countInColumn, int initY, float fontSize) {
-        Point result = new Point();
-        result.x = (int)(fontStartXInView - (fontSize + COLUMN_WIDTH) * (index / countInColumn + 1));
-        result.y = (int)((fontSize + ROW_HEIGHT) * (index % countInColumn) + initY);
-        return result;
-    }
-    private Point measureCharHorizontalPosition(int index, int fontStartXInView, int countInRow, int initY, float fontSize) {
-        //        Rect textBounds = new Rect();
-        //        paint.getTextBounds(String.valueOf(c), 0, 1, textBounds);
+    private Point measureCharHorizontalPosition(char c,
+                                                int lastX, int lastY,
+                                                int widthPadding, int heightPadding,
+                                                boolean isVertical, int fontSize,
+                                                int viewWidth, int viewHeight) {
+        int addToOrientation;// 加载文字方向 0右，1下，2左
 
-        Point result = new Point();
-        // 计算出当前文字区域
-        result.x = (int)((fontSize + COLUMN_WIDTH) * (index % countInRow) + fontStartXInView);
-        result.y = (int)((fontSize + ROW_HEIGHT) * (index / countInRow) + initY);
+        if(NEW_LINE_CHAR == c) {
+            addToOrientation = isVertical ? 2 : 1;
+            if(isVertical) {
+                lastY = 0;
+            } else {
+                lastX = 0;
+            }
+        } else {
+            addToOrientation = isVertical ? 1 : 0;
+        }
 
-        return result;
+        int newX = lastX;
+        int newY = lastY;
+        switch (addToOrientation) {
+            case 0:
+                newX = lastX + fontSize + widthPadding;
+                break;
+            case 1:
+                newY = lastY + fontSize + (isVertical ? widthPadding : heightPadding);
+                break;
+            case 2:
+                newX = lastX - fontSize - heightPadding;
+                break;
+        }
+
+        // 越界换行
+        if(newX > viewWidth || newX < 0 || newY > viewHeight) {
+            if(isVertical) {
+                newX = lastX - fontSize - heightPadding;
+                newY = 0;
+            } else {
+                newX = 0;
+                newY = lastX - fontSize - heightPadding;
+            }
+        }
+
+        // 换行后仍旧越界
+        if(newX > viewWidth || newX < 0 || newY > viewHeight) {
+            return null;
+        }
+        return new Point(newX, newY);
     }
 
     public void Log(String content) {
